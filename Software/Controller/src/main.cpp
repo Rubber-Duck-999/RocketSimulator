61632e8c5da09081e018350f59b802c38a147db3
Simon Crowther <simon.crowther95@hotmail.co.uk>
2019-03-16 18:06:47 +0000

    Update to controller, currently issue with socket comms at right time. May switch to udp

--- Software/Controller/src/main.cpp
+++ Software/Controller/src/main.cpp
@@ -6,14 +6,15 @@ using namespace std;
 
 class Socket
 {
+public:
 	Socket(int Port):_port(Port) {}
     void createSocket();
-    void sendData();
+    void receiveFromConsole(string& receive);
+    void sendToGraphics();
     void closeSocket();
-
+private:
 	int _port;
 	bool leave = false;
-    std::string networkIPAddress = "127.0.0.1";
     WSADATA WSAData;
     SOCKET server;
     SOCKADDR_IN addr;
@@ -24,21 +25,46 @@ void Socket::createSocket()
     WSAStartup(MAKEWORD(2,0), &WSAData);
     server = socket(AF_INET, SOCK_STREAM, 0);
 
-    addr.sin_addr.s_addr = networkIPAddress;
+    addr.sin_addr.s_addr = inet_addr("127.0.0.1");
     addr.sin_family = AF_INET;
     addr.sin_port = htons(_port);
 }
 
 
-void Socket::sendData()
+void Socket::receiveFromConsole(string& receive)
 {
-    connect(server, (SOCKADDR *)&addr, sizeof(addr));
-    cout << "Connected to server!" << endl;
     while(!leave)
     {
+        connect(server, (SOCKADDR *)&addr, sizeof(addr));
+        cout << "Connected to server!" << endl;
         char recvBuffer[1024];
         recv(server, recvBuffer, sizeof(recvBuffer), 0);
+        if(recvBuffer[0] == 'M')
+        {
+            cout << "We have received: " << recvBuffer << endl;
+            cout << "Sending Acknowledgement" << endl;
+        	char buffer[1024]={'A', 'C', 'K', '_', recvBuffer[0], '_', recvBuffer[3], recvBuffer[4], recvBuffer[5]};
+            send(server, buffer, sizeof(buffer), 0);
+            cout << "Message sent!" << endl;
+            receive = recvBuffer;
+            leave = true;
+        }
+    }
+    closesocket(server);
+    WSACleanup();
+    cout << "Socket closed." << endl << endl;
+}
 
+
+void Socket::sendToGraphics()
+{
+    while(!leave)
+    {
+        connect(server, (SOCKADDR *)&addr, sizeof(addr));
+        cout << "Connected to server!" << endl;
+        char recvBuffer[1024];
+        recv(server, recvBuffer, sizeof(recvBuffer), 0);
+        cout << "We have received: " << recvBuffer << endl;
         if(recvBuffer[0] == 'M')
         {
         	char buffer[1024]={'A', 'C', 'K', '_'};
@@ -47,10 +73,6 @@ void Socket::sendData()
             leave = true;
         }
     }
-}
-
-void Socket::closeSocket()
-{
     closesocket(server);
     WSACleanup();
     cout << "Socket closed." << endl << endl;
@@ -59,7 +81,12 @@ void Socket::closeSocket()
 
 int main()
 {
-	std::cout << "Starting Controller" << std::endl;
-
+	cout << "Starting Controller" << endl;
+    Socket consoleSocket(6111);
+    consoleSocket.createSocket();
+    string consoleData;
+    consoleSocket.receiveFromConsole(consoleData);
+    cout << consoleData << endl;
+    //consoleSocket.closeSocket();
 	return 0;
 }
