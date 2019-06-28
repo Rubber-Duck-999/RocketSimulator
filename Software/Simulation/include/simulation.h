9a7df8ec273de193cdce95de52fec33049475083
simon <simon.crowther95@hotmail.co.uk>
2019-06-28 21:11:03 +0100

    Added Simulation additions

--- Software/Simulation/include/simulation.h
+++ Software/Simulation/include/simulation.h
@@ -10,7 +10,7 @@ class Simulator
 public:
     Simulator() { };
     //
-    void CreateRocketSimulation(rocket_simulator::LauncherMissionParameters& launcher) { };
+    void CreateRocketSimulation(rocket_simulator::LauncherMissionParameters& launcher);
     //
 };
 
