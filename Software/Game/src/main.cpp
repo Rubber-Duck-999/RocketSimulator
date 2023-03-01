ae6758bcbf0ef602983c8a4a2ff53d206d61c66a
rubber-duck-999 <simon.crowther95@hotmail.co.uk>
2023-03-01 21:32:06 +0000

    Remove asteroids as it doesn't run

--- Software/Game/src/main.cpp
+++ Software/Game/src/main.cpp
@@ -1,72 +1,48 @@
-// Core
-#include <iostream>
-#include <vector>
-#include <stdio.h>
-#include <math.h>
-#include <cstdlib>
-//Allegro
-#include <allegro5/allegro.h>
-#include <allegro5/allegro_primitives.h>
+#include <allegro5/allegro5.h>
 #include <allegro5/allegro_font.h>
-#include <allegro5/allegro_ttf.h>
-// Self
-#include "init.h"
-#include "objects.h"
-#include "config.h"
-#include "engine.h"
+#include <stdbool.h>
 
-using namespace std;
-
-int main(int argc, char **argv)
+int main()
 {
+    al_init();
+    al_install_keyboard();
 
-    srand(static_cast<unsigned int>(time(0)));
-    
+    ALLEGRO_TIMER* timer = al_create_timer(1.0 / 30.0);
+    ALLEGRO_EVENT_QUEUE* queue = al_create_event_queue();
+    ALLEGRO_DISPLAY* disp = al_create_display(840, 600);
+    ALLEGRO_FONT* font = al_create_builtin_font();
+
+    al_register_event_source(queue, al_get_keyboard_event_source());
+    al_register_event_source(queue, al_get_display_event_source(disp));
+    al_register_event_source(queue, al_get_timer_event_source(timer));
+
     bool redraw = true;
-    bool doexit = false;
-    if (!InitAllegro()) {
-        return -1;
+    ALLEGRO_EVENT event;
+
+    al_start_timer(timer);
+    while(1)
+    {
+        al_wait_for_event(queue, &event);
+
+        if(event.type == ALLEGRO_EVENT_TIMER)
+            redraw = true;
+        else if((event.type == ALLEGRO_EVENT_KEY_DOWN) || (event.type == ALLEGRO_EVENT_DISPLAY_CLOSE))
+            break;
+
+        if(redraw && al_is_event_queue_empty(queue))
+        {
+            al_clear_to_color(al_map_rgb(0, 0, 0));
+            al_draw_text(font, al_map_rgb(255, 255, 255), 0, 0, 0, "Hello world!");
+            al_flip_display();
+
+            redraw = false;
+        }
     }
 
-    InitGame();
+    al_destroy_font(font);
+    al_destroy_display(disp);
+    al_destroy_timer(timer);
+    al_destroy_event_queue(queue);
 
-    al_flip_display();
-    al_start_timer(timer);
-
-    while(!doexit) {
-        ALLEGRO_EVENT event;
-        al_wait_for_event(event_queue, &event);
- 
-        if(event.type == ALLEGRO_EVENT_TIMER) {
-            if ((doexit = !GameContinues())) {
-                break;
-            } else {
-            DispatchKeys();
-            UpdateObjects();
-            redraw = true;
-            }
-        }
-
-        // press a key
-        else if(event.type == ALLEGRO_EVENT_KEY_DOWN) {
-            KeyDown(event);
-        }
-        // release a key
-        else if(event.type == ALLEGRO_EVENT_KEY_UP) {
-            doexit = KeyUp(event);            
-        }
-        // close display
-        if(event.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
-            break;
-        }
-        // redraw
-        if(redraw && al_is_event_queue_empty(event_queue)) {
-            redraw = false;
-            Redraw();
-        }
-    } 
-
-    al_destroy_display(display);
-    Cleanup();    
     return 0;
 }
