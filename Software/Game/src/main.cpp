cf17f25e89b175502d7a1bf586ffc522361223a6
rubber-duck-999 <simon.crowther95@hotmail.co.uk>
2023-03-03 09:08:45 +0000

    Added common init_game

--- Software/Game/src/main.cpp
+++ Software/Game/src/main.cpp
@@ -1,43 +1,77 @@
+#include <stdio.h>
 #include <allegro5/allegro5.h>
 #include <allegro5/allegro_font.h>
-#include <stdbool.h>
+#include <allegro5/allegro_image.h>
 
-int main()
-{
-    al_init();
-    al_install_keyboard();
+void must_init(bool test, const char *description) {
+    if(test) return;
+
+    printf("couldn't initialize %s\n", description);
+    exit(1);
+}
+
+int main() {
+    must_init(al_init(), "allegro");
+    must_init(al_install_keyboard(), "keyboard");
 
     ALLEGRO_TIMER* timer = al_create_timer(1.0 / 30.0);
+    must_init(timer, "timer");
+
     ALLEGRO_EVENT_QUEUE* queue = al_create_event_queue();
-    ALLEGRO_DISPLAY* disp = al_create_display(840, 600);
+    must_init(queue, "queue");
+
+    ALLEGRO_DISPLAY* disp = al_create_display(1920, 1080);
+    must_init(disp, "display");
+
     ALLEGRO_FONT* font = al_create_builtin_font();
+    must_init(font, "font");
+
+    must_init(al_init_image_addon(), "image addon");
+    ALLEGRO_BITMAP* milkyway = al_load_bitmap("../src/milkyway.jpg");
+    must_init(milkyway, "milkyway");
 
     al_register_event_source(queue, al_get_keyboard_event_source());
     al_register_event_source(queue, al_get_display_event_source(disp));
     al_register_event_source(queue, al_get_timer_event_source(timer));
 
+    bool done = false;
     bool redraw = true;
     ALLEGRO_EVENT event;
 
     al_start_timer(timer);
-    while(1)
-    {
+    while(1) {
         al_wait_for_event(queue, &event);
 
-        if(event.type == ALLEGRO_EVENT_TIMER)
-            redraw = true;
-        else if((event.type == ALLEGRO_EVENT_KEY_DOWN) || (event.type == ALLEGRO_EVENT_DISPLAY_CLOSE))
+        switch(event.type) {
+            case ALLEGRO_EVENT_TIMER:
+                // game logic goes here.
+                redraw = true;
+                break;
+
+            case ALLEGRO_EVENT_KEY_DOWN:
+            case ALLEGRO_EVENT_DISPLAY_CLOSE:
+                done = true;
+                break;
+        }
+
+        if(done) {
             break;
+        }
 
         if(redraw && al_is_event_queue_empty(queue))
         {
             al_clear_to_color(al_map_rgb(0, 0, 0));
             al_draw_text(font, al_map_rgb(255, 255, 255), 0, 0, 0, "Hello world!");
+
+            al_draw_bitmap(milkyway, 0, 0, 0);
+
             al_flip_display();
 
             redraw = false;
         }
     }
+
+    al_destroy_bitmap(milkyway);
 
     al_destroy_font(font);
     al_destroy_display(disp);
